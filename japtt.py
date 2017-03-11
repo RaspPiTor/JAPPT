f9c560893ce47bee8b483a9bff42286a29b9404a
Hugh Williams <hughaw64@gmail.com>
2017-03-11 11:14:38 +0000

    Followed style guidelines

--- japtt.py
+++ japtt.py
@@ -1,13 +1,13 @@
+import cmd
+
 from XSS.xssencode import xssencode
 from CI.ciencode import ciencode
 
-import cmd
-
 import logger
 
-logger=logger.Logger()
+LOGGER = logger.Logger()
 class JapptShell(cmd.Cmd):
-    intro=logger.colour('''
+    intro = LOGGER.colour(r'''
      _   _    ____ _____ _____ 
     | | / \  |  _ \_   _|_   _|
  _  | |/ _ \ | |_) || |   | |  
@@ -16,23 +16,23 @@ class JapptShell(cmd.Cmd):
  Just Another Pen Testing Tool
  
  ''', colour=31, bold=1)
-    intro+=logger.colour('Welcome to JAPTT. Type help or ? to list commands.\n',
-                         colour=32, bold=1)
-    prompt=logger.colour('(japtt)', 32, bold=1)+logger.colour('>>>', 34, bold=1)
-    file=None
-    ruler=logger.colour('=', colour=32, bold=1)
+    intro += LOGGER.colour('Welcome to JAPTT. Type help or ? to list commands.\n',
+                           colour=32, bold=1)
+    prompt = LOGGER.colour('(japtt)', 32, bold=1) + LOGGER.colour('>>>', 34, bold=1)
+    file = None
+    ruler = LOGGER.colour('=', colour=32, bold=1)
     def emptyline(self):
         pass
     def default(self, line):
-        logger.error('Unknown syntax: '+line)
+        LOGGER.error('Unknown syntax: '+line)
     def do_print(self, args):
         print(args)
-    def do_quit(self, args):
+    def do_quit(self, _):
         'Exit from the Jappt shell'
-        logger.info('Exiting')
+        LOGGER.info('Exiting')
         exit()
-    def do_EOF(self, args):
-        logger.info('Exiting')
+    def do_EOF(self, _):
+        LOGGER.info('Exiting')
         exit()
     def do_xssencode(self, args):
         'Put inputted javascript into a tag which could cause Cross Site Scripting'
@@ -44,4 +44,4 @@ if __name__ == '__main__':
     try:
         JapptShell().cmdloop()
     except KeyboardInterrupt:
-        logger.info('Exiting')
+        LOGGER.info('Exiting')
